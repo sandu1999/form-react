f84ca41797a88f5d0d0267177a98698c9feef5f9
sandu1999 <99947269+sandu1999@users.noreply.github.com>
2024-02-05 21:28:52 +0530

    lol

--- src/Components/LoginSignup/LoginSignup.js
+++ src/Components/LoginSignup/LoginSignup.js
@@ -1,29 +1,62 @@
 import React from 'react'
 import './LoginSignup.css'
-import email_icon from '../Assests/email.png'
-import password_icon from '../Assests/password.png'
+import facebook_icon from '../Assests/icons8-facebook-48.png'
+import google_icon from '../Assests/icons8-google-48.png'
+import twitter_icon from '../Assests/icons8-twitter-48.png'
+import picture from '../Assests/sign-page-abstract-concept-illustration_335657-3875.avif'
+import cover from '../Assests/new.png'
+
+
 
 const LoginSignup = () => {
+  
+  
   return (
    <div className='main-container'>
+  {/* <img src={cover} alt=''/> */}
+    
     <div className='left-side'>
     <div className='creative'><span>C</span>reative</div>
+    <img src={picture} alt=''/>
     </div>
+    
+    
     <div className='rigt-side'>
     <div className='new-user'>new User?<span>sign up</span></div>
-    </div>
     <div className='welcome'>Wecome Back!</div>
     <div className='login'>Login to continue</div>
     <div className='inputs'>
       <div className='input'>
-        <img src={email_icon} alt=''/>
+        
         <input type='email' placeholder='Enter Email'/>
       </div>
       <div className='input'>
-        <img src={password_icon} alt=''/>
+       
         <input type='password' placeholder='Enter Password'/>
       </div>
+      
+
+</div>
+      <div className='last'>
+      <button className='button'>Login</button>
+      <p className='para'>foget password?</p>
       </div>
+      <div className='final'>
+      <p className='lo'>Login with</p>
+      <div className='img'>
+      <img src={facebook_icon} alt=''/>
+      <img src={twitter_icon} alt=''/>
+      <img src={google_icon} alt=''/>
+      </div>
+      </div>
+        
+     
+   
+     
+      
+     
+    </div>
+  
 
 
    </div>
